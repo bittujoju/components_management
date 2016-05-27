b1687ff77b74f9df57fc3f419fee72f3253c9078
Dittu Joju <Dittu.Joju@VERIFONE.com>
2016-05-26 20:44:35 -0700

    Depend and Install done

--- components_management.rb
+++ components_management.rb
@@ -10,13 +10,11 @@ module ComponentsManagement
       @parents = []
       @children = []
       @@component_instances_list << self
-      puts "created"
     end
 
     def add_child(c)
-      if @children == nil
-      	puts "value is #{@children}"
-        @chilren << c unless @children.include? c
+      if @children.any?
+        @children << c unless @children.include? c
       else
       	@children = [c]
       end
@@ -27,7 +25,7 @@ module ComponentsManagement
     end
 
     def add_parent(p)
-      if @parents
+      if @parents.any?
         @parents << p unless @parents.include? p
       else
       	@parents = [p]
@@ -42,51 +40,75 @@ module ComponentsManagement
       @@component_instances_list.each do |comp|
         if comp.name == name
         	return comp
-        else
-        	return nil
         end
       end
       return nil
     end
 
-    def self.show
-    	puts "total numbe of components: #{@@component_instances_list.size}"
-    	@@component_instances_list.each do |comp|
-    	  puts "NAME: #{comp.name}"
-    	  puts "PARENTS: #{comp.parents.each {|p| puts p}}"
-    	  puts "CHILDREN: #{comp.children.each {|c| puts c}}"
-    	end
+    def self.display_array(arr)
+      result = ""
+      arr.each do |a|
+        result << " " << a
+      end
+      puts result
     end
 
-  end 
-  LIST = []
+    def self.show
+    	@@component_instances_list.each do |comp|
+    	  puts "\nNAME: #{comp.name}"
+    	  puts "PARENTS: #{comp.parents}"
+    	  puts "CHILDREN: #{comp.children}"
+      end
+      puts "total number of components: #{@@component_instances_list.size}"
+    end
+  end
+
+  $LIST = []
   sample_input = []
   File.open('sample_input.txt').each do |line|
-    sample_input  << line.split(" ")
+    words = line.split(" ")
+    sample_input  << words
   end
 
   sample_input.each do |command|
     case command[0]
     when 'DEPEND'
-    	for index in 1..command.size
-          comp = Component.find_by(command[index])
-          comp = Component.new(command[index]) unless comp
-          for parent_index in (index+1)..command.size
-          	comp.add_parent((command[parent_index])) if command[parent_index]
+      Component.display_array(command)
+      for index in 1...command.size
+        comp = Component.find_by(command[index])
+        comp = Component.new(command[index]) unless comp
+        for parent_index in (index+1)...command.size
+          if command[parent_index]
+            comp.add_parent((command[parent_index]))
+            parent_comp = Component.find_by(command[parent_index])
+            parent_comp = Component.new(command[parent_index]) unless parent_comp
           end
         end
-        for second_index in (command.size).downto(1)
-        	comp = Component.find_by(command[second_index])
-        	comp = Component.new(command[second_index]) unless comp
-        	for child_index in (index-1).downto(1)
-          	  comp.add_child(command[child_index]) if command[child_index]
-            end
+        for child_index in (index-1).downto(1)
+          if command[child_index]
+            comp.add_child(command[child_index])
+          end
         end
-
+      end
+    when 'INSTALL'
+      Component.display_array(command)
+      unless $LIST.include? command[1]
+        comp = Component.find_by(command[1])
+        if comp
+          comp.parents.each do |parent|
+            unless $LIST.include? parent
+              puts "\tInstalling #{parent}"
+              $LIST << parent
+            end
+          end
+        else
+          comp = Component.new(command[1]) unless comp
+        end
+        puts "\tInstalling #{comp.name}"
+        $LIST << comp.name
+      else
+        puts "\t#{command[1]} is already installed"
+      end
     end
-
-  Component.show
-
   end
-
 end
